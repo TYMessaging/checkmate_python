341abafd1f280c261c297c8fd587a5accc0e0c92
ryane <ryanesc@gmail.com>
2014-12-18 15:59:27 -0700

    clean up Reservations endpoint

--- checkmate/reservations.py
+++ checkmate/reservations.py
@@ -2,31 +2,15 @@ class Reservations:
     def __init__(self, client):
         self.client = client
 
-    def list(self,
-             page=1,
-             property_id=None,
-             confirmation_number=None,
-             exclude_properties=False):
-
-        params = {'page': page}
-        url = '/reservations'
-
-        if property_id is not None:
-            url = '/properties/{id}/reservations'.format(id=property_id)
-            params['property_id'] = property_id
-
-        if confirmation_number is not None:
-            params['confirmation_number'] = confirmation_number
-
-        if exclude_properties:
-            params['exclude_properties'] = 'true'
-
-        return self.client.request('GET', url, params)
+    def list(self, params={}):
+        return self.client.request('GET', self._url(params), params)
 
     def create(self, params={}):
-        url = '/reservations'
+        return self.client.request('POST', self._url(params), params)
+
+    def _url(self, params={}):
         if 'property_id' in params:
             property_id = params['property_id']
-            url = '/properties/{id}/reservations'.format(id=property_id)
-
-        return self.client.request('POST', url, params)
+            return '/properties/{id}/reservations'.format(id=property_id)
+        else:
+            return '/reservations'
