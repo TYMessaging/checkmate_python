6bdadad17674e73011aee3ce9472d65ff0bdccb0
ryane <ryanesc@gmail.com>
2014-12-18 11:21:24 -0700

    cleanup and pep8 fixes

--- checkmate/checkmate.py
+++ checkmate/checkmate.py
@@ -9,8 +9,10 @@ except ImportError:
     except ImportError:
         import json
 
+
 class CheckMate:
-    def __init__(self, api_key=None, api_base='http://partners.checkmate.dev:3000'):
+    def __init__(self, api_key=None,
+                 api_base='http://partners.checkmate.dev:3000'):
         self.api_key = api_key
         self.api_base = api_base
         self.session = requests.session()
@@ -19,25 +21,23 @@ class CheckMate:
             'Accept': 'application/json'
         })
 
-    def list_reservations(self):
-        reservations =  self._request('GET', '/reservations').reservations
+    def list_reservations(self, params={}):
+        reservations = self._request('GET', '/reservations', params)
         return reservations
 
-    def _request(self, method, url, params=None):
-        if params is None: params = {}
-
-        response = self.session.request(method, self._request_uri(url))
+    def _request(self, method, url, params={}):
+        response = self.session.request(method,
+                                        self._request_uri(url),
+                                        data=params)
         if response.status_code != requests.codes.ok:
-            response.raise_for_status()           
+            response.raise_for_status()
 
         result = response.json()
 
         if isinstance(result, list):
-            return [ AttrDict(r) for i in result ]
+            return map(lambda r: AttrDict(r), result)
         else:
             return AttrDict(result)
 
     def _request_uri(self, url):
-        return '%s%s' % (self.api_base, url)
-
-
+        return '{root}{url}'.format(root=self.api_base, url=url)
